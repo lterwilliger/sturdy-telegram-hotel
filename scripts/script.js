4eac9eea56a7b299dff4abafa371030b7defed20
Luke Terwilliger <66847110+lterwilliger@users.noreply.github.com>
2022-03-30 16:06:20 -0400

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,20 @@
 $(document).ready(function() {
     console.log("document is ready!");
+    // Get element info
     var el = document.getElementById("submitButton");
     var elName = document.getElementById("username");
 
+// clear button function calls
     $('#btnClear').click(function(){
             clearElement("#username", "input");
             clearElement("#phone", "input");
-            clearElement("#birthdate", "input");
-            clearElement("#birthdate", "input");
+            clearElement("#checkIn", "input");
+            clearElement("#checkOut", "input");
             clearElement("#password", "input");
             clearElement("#username", "input");
 
       })
-
+// Clear element by type
     function clearElement(elementId, elementType){
         if(elementType == "html"){
             $(elementId).html("");
@@ -20,8 +22,11 @@ $(document).ready(function() {
         else if(elementType=="input"){
             $(elementId).val(" ");
         }
+        else {
+          $(elementId).val(" ");
+        }
     }
-    // need some form of flow control
+  
 
 
 
