da746bced8362b553e7a84f3e7463f816d15f9fd
Luke Terwilliger <66847110+lterwilliger@users.noreply.github.com>
2022-04-06 11:04:31 -0400

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
     console.log("document is ready!");
     // Get element info
+    var frst = document.getElementById("firstTime");
+    var retr = document.getElementById("againTime");
+    frst.style.display = "none";
+    retr.style.display = "none";
+
     var el = document.getElementById("submitButton");
     var elName = document.getElementById("username");
 
@@ -13,7 +18,41 @@ $(document).ready(function() {
             clearElement("#password", "input");
             clearElement("#username", "input");
 
-      })
+      });
+      $('input:radio[name="radioDefault"]').change(
+        function(){
+          if ($(this).is(':checked') && $(this).val() == 'first' ) {
+            //console.log("first is checked");
+              var frs = document.getElementById("firstTime");
+              var again = document.getElementById("againTime");
+              if (frs.style.display === "none") {
+                frs.style.display = "block";
+                again.style.display = "none";
+              } else {
+                frs.style.display = "none";
+              }
+          }
+          else if ($(this).is(':checked') && $(this).val() == 'return' ) {
+            var frs = document.getElementById("firstTime");
+            var again = document.getElementById("againTime");
+            if (again.style.display === "none") {
+              again.style.display = "block";
+              frs.style.display = "none";
+            } else {
+              again.style.display = "none";
+            }
+          }
+          else {
+            console.log("Something else");
+          }
+          //   var x = document.getElementById("firstTime");
+          //   if (x.style.display === "none") {
+          //     x.style.display = "block";
+          //   } else {
+          //     x.style.display = "none";
+          //   }
+          // }
+    });
 // Clear element by type
     function clearElement(elementId, elementType){
         if(elementType == "html"){
@@ -26,10 +65,6 @@ $(document).ready(function() {
           $(elementId).val(" ");
         }
     }
-  
-
-
-
 
 
 
